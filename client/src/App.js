1010f2281a6d450772f07e754c081a004f0d0ca6
Rodolfo Silva <rasilvap22@gmail.com>
2019-12-30 22:38:45 -0500

    RS:graphql-api: Added books compoents and queries file.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 // components
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 // apollo client setup
 const client = new ApolloClient({
@@ -15,6 +16,7 @@ function App() {
       <div id="main">
         <h1>Rodolfo Silva.</h1>
         <BookList />
+        <AddBook />
       </div>
   </ApolloProvider>
   );
